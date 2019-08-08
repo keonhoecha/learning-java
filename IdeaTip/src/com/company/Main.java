76d5600f6075895fd69ae450721085212b72d4c8
Keonhoe Cha <kcha@jimmys-imac.attlocal.net>
2019-08-07 21:54:01 -0700

    initial commit

---
+++ IdeaTip/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        MyStudent.printStudent();
+    }
+
+    void methodA() {
+        System.out.println("methodA is called");
+
+    }
+
+    void methodB() {
+        for(int i=0; i<10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("methodB is called.");
+    }
+}
+
+class MyStudent {
+    public static void printStudent() {
+        System.out.println("hello");
+    }
+}
